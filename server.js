7c3eac28714b974979ec4b2c240ef918b97acc44
amanrkt2020@gmail.com <amanrkt2020@gmail.com>
2025-06-11 23:31:55 +0530

    Refactor server.js to serve static files and return index.html on root request

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 import express from 'express';
 import { createServer } from 'http'
-
+import { dirname, join } from 'path'
+import { fileURLToPath } from 'url'
 const app = express();
 const server = createServer(app)
 
-app.get('/',(req, res)=>{
-res.send('Server Running on port 3000')
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+app.use(express.static(join(__dirname, 'public')))
+app.get('/', (req, res) => {
+
+  //dirname(fileURLToPath(import.meta.url))
+
+  res.sendFile(join(__dirname + '/public' + '/index.html'))
 })
 
-server.listen('3000',()=>{
+server.listen('3000', () => {
+  console.log(dirname(fileURLToPath(import.meta.url)))
   console.log(`[RUNNING] -->  Server is running on port 3000 : http://localhost:3000`)
 });
